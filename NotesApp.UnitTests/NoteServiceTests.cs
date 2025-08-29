3192a1bca21b3475bb0ab44538f8fbdbb03fc5a7
kaansrflioglu <kaansereflioglu@hotmail.com>
2025-08-29 14:44:42 +0300

    test(service): CRUD işlemlerini denetleyecek testler eklendi
    - Testlerde mock veritabanı kullanıldı
    - CRUD işlemlerinin hepsini denetleyecek 4 test oluşturuldu

---
+++ NotesApp.UnitTests/NoteServiceTests.cs
@@ -0,0 +1,56 @@
+﻿using NotesApp.Application.Interfaces;
+using NotesApp.Domain.Entities;
+using Moq;
+using Xunit;
+
+namespace NotesApp.UnitTests
+{
+    public class NoteRepositoryTests
+    {
+        [Fact]
+        public async Task AddNote_CallsAddAsync()
+        {
+            var mockRepo = new Mock<INoteRepository>();
+            var note = new Note { Title = "New Note", Content = "Content" };
+
+            await mockRepo.Object.AddAsync(note);
+
+            mockRepo.Verify(r => r.AddAsync(note), Times.Once);
+        }
+
+        [Fact]
+        public async Task UpdateNote_CallsUpdateAsync()
+        {
+            var mockRepo = new Mock<INoteRepository>();
+            var note = new Note { Id = Guid.NewGuid(), Title = "Update", Content = "Content" };
+
+            await mockRepo.Object.UpdateAsync(note);
+
+            mockRepo.Verify(r => r.UpdateAsync(note), Times.Once);
+        }
+
+        [Fact]
+        public async Task GetNoteById_ReturnsNote()
+        {
+            var id = Guid.NewGuid();
+            var mockRepo = new Mock<INoteRepository>();
+            mockRepo.Setup(r => r.GetByIdAsync(id))
+                    .ReturnsAsync(new Note { Id = id, Title = "Test", Content = "Content" });
+
+            var note = await mockRepo.Object.GetByIdAsync(id);
+            Assert.NotNull(note);
+            Assert.Equal(id, note.Id);
+        }
+
+        [Fact]
+        public async Task DeleteNote_CallsDeleteAsync()
+        {
+            var id = Guid.NewGuid();
+            var mockRepo = new Mock<INoteRepository>();
+
+            await mockRepo.Object.DeleteAsync(id);
+
+            mockRepo.Verify(r => r.DeleteAsync(id), Times.Once);
+        }
+    }
+}
